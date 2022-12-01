8378e032cb212bb177a28297c1b4f359a5d9fa91
Ferhat Omur <fomur1@jhu.edu>
2022-12-01 15:59:27 -0500

    Interactive console version

--- 2022/AdventOfCode2022/Day01.cs
+++ 2022/AdventOfCode2022/Day01.cs
@@ -20,5 +20,14 @@ public static class Day01
             g.Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(snackCals => Convert.ToInt32(snackCals.Trim()))
                 .Sum()).OrderByDescending(i => i);
         Console.WriteLine(calories.Take(3).Sum());
+
+        var result = File.ReadAllText(@"InputFiles\D01P01.txt").Split("\n\n")
+            .Select(
+                g => g.Split("\n", StringSplitOptions.RemoveEmptyEntries)
+                .Select(snackCals => Convert.ToInt32(snackCals.Trim()))
+                .Sum()
+            ).OrderByDescending(i => i)
+            .Take(3)
+            .Sum();
     }
 }
--- 2022/AdventOfCode2022/_Main.cs
+++ 2022/AdventOfCode2022/_Main.cs
@@ -1,2 +1,3 @@
 ﻿using AdventOfCode2022;
+Day01.P01();
 Day01.P02();
